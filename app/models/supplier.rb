3d055559d0e96009e8c0e09c6937e10a5408784f
Pierre Cohen <pierre-cohen@hotmail.fr>
2018-05-29 17:05:22 +0200

    availabilty model + associations good

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,8 +1,11 @@
 class Supplier < ApplicationRecord
-  has_many :bundle_line
-  has_many :supplier_area
   belongs_to :service
+  has_many :bundle_lines
+  has_many :supplier_areas
   has_many :reviews
+  has_many :availabilities
+  has_many :areas, through: :supplier_areas
+  has_many :users, through: :reviews
   validates :owner, :description, :email, presence: true
   validates :address, :capacity, presence: true, if: :is_a_place?
 
