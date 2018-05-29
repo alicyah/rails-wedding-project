1c286def89eb546b32535b57d394f2480991fa49
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-29 16:29:41 +0200

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,25 @@ ActiveRecord::Schema.define(version: 2018_05_29_122912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "areas", force: :cascade do |t|
+    t.string "country"
+    t.string "region"
+    t.string "department"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bundles", force: :cascade do |t|
+    t.string "state"
+    t.date "starts_on"
+    t.date "ends_on"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bundles_on_user_id"
+  end
+
   create_table "services", force: :cascade do |t|
     t.string "category"
     t.string "name"
@@ -34,25 +53,6 @@ ActiveRecord::Schema.define(version: 2018_05_29_122912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["service_id"], name: "index_suppliers_on_service_id"
-  end
-    
-  create_table "areas", force: :cascade do |t|
-    t.string "country"
-    t.string "region"
-    t.string "department"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-  
-  create_table "bundles", force: :cascade do |t|
-    t.string "state"
-    t.date "starts_on"
-    t.date "ends_on"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_bundles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,6 +74,6 @@ ActiveRecord::Schema.define(version: 2018_05_29_122912) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "suppliers", "services"
   add_foreign_key "bundles", "users"
+  add_foreign_key "suppliers", "services"
 end
