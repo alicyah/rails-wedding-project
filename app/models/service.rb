2d8cba7cb61e9363ae2ef67a539ae3dd037bc331
Alicia Yahiaoui <alicia.yahiaoui@gmail.com>
2018-05-29 12:58:32 +0200

    corrected typo

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,4 @@
-class Service < ApplicationRecord$
+class Service < ApplicationRecord
   has_many :suppliers
   validates :category, presence: true, uniqueness: { scope: :name }
   validates :name, presence: true
