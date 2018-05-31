5a54c9cb5829ba63c77d5fc3b0336fe0b301d66a
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-31 16:25:14 +0200

    added a new migration in the bundle model and started back end code of bundles new

--- app/controllers/bundles_controller.rb
+++ app/controllers/bundles_controller.rb
@@ -3,9 +3,20 @@ class BundlesController < ApplicationController
   end
 
   def new
+    @bundle = Bundle.new
   end
 
   def create
+    session[:bundle] = {}
+    category-name = []
+    @bundle.user = current.user
+    @bundle.save
+
+  if
+    redirect_to location_bundle_path(@bundle)
+  else
+    redirect_to services_bundle_path(@bundle)
+  end
   end
 
   def location
