3ef54447253ddaa9b5024ba11a836575ca834c37
Yasmina Saradar <yasminasaradar@gmail.com>
2018-06-04 16:34:59 +0200

    ok

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,10 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 import { French } from "flatpickr/dist/l10n/fr.js"
 
-flatpickr(".start_date", {
+flatpickr(".datepicker", {
   altInput: true,
   locale: 'fr',
   ariaDateFormat: 'j f Y',
-  plugins: [new rangePlugin({ input: ".end_date"})]
 })
