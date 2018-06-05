d14db3e6102e75b09aa5767965e70ecb0fbe1c92
Pierre Cohen <pierre-cohen@hotmail.fr>
2018-06-05 17:12:31 +0200

    design + create ok sur service page. check comment of <script> pour demain aprèm

--- app/controllers/bundle_lines_controller.rb
+++ app/controllers/bundle_lines_controller.rb
@@ -1,12 +1,21 @@
 class BundleLinesController < ApplicationController
   def create
     @bundle = Bundle.find(params[:bundle_id])
-    @bundle_line = BundleLine.new(bundle: @bundle)
-    @supplier = Supplier.find(params[:id])
-    @bundle_line.bundle = @bundle
-    @bundle_line.supplier = @supplier
+    if params[:supplier_ids]
+      ids = params[:supplier_ids].split
+      ids.each do |supplier_id|
+        @bundle_line = BundleLine.new(bundle: @bundle)
+        @supplier = Supplier.find(supplier_id)
+        @bundle_line.supplier = @supplier
+        @bundle_line.save
+      end
+    else
+      @bundle_line = BundleLine.new(bundle: @bundle)
+      @supplier = Supplier.find(params[:id])
+      @bundle_line.supplier = @supplier
+    end
     if @bundle_line.save
-      if session[:bundle]["categories"].include?("location") && session[:bundle]["categories"].count > 1
+      if session[:bundle]["categories"].include?("lieu") && session[:bundle]["categories"].count > 1
         redirect_to services_bundle_path(@bundle)
       else
         redirect_to bundle_path(@bundle)
@@ -14,7 +23,5 @@ class BundleLinesController < ApplicationController
     else
       redirect_to location_bundle_path(@bundle)
     end
-
   end
-
 end
