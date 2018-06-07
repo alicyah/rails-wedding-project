6303718fdf9a021ccd482d0e347c8bb09893971d
Alicia Yahiaoui <alicia.yahiaoui@gmail.com>
2018-06-07 20:24:17 +0200

    js tu display descriptions

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -16,11 +16,19 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
       const place_price = mapMarker.place_price;
 
       // change slideShow
-      const photoToHide = document.querySelector('.supplier-photo:not(.hidden)')
-      photoToHide.classList.add('hidden')
-      const photoToDisplay = document.getElementById(`supplier-photo-${id}`)
-      photoToDisplay.classList.remove('hidden')
-      console.log(mapMarker);
+      const photoToHide = document.querySelector('.supplier-photo:not(.hidden)');
+      photoToHide.classList.add('hidden');
+      const photoToDisplay = document.getElementById(`supplier-photo-${id}`);
+      photoToDisplay.classList.remove('hidden');
+      const detailsToHide = document.querySelector('.supplier-details:not(.hidden)');
+      detailsToHide.classList.add('hidden');
+      const detailsToDisplay = document.getElementById(`supplier-details-${id}`);
+      detailsToDisplay.classList.remove('hidden');
+      const descriptionToHide = document.querySelector('.supplier-description:not(.hidden)');
+      descriptionToHide.classList.add('hidden');
+      const descriptionToDisplay = document.getElementById(`supplier-description-${id}`);
+      descriptionToDisplay.classList.remove('hidden');
+
       // fill the form with new value of supplier_id
       const input = document.querySelector("form #supplier_id");
       input.value = id;
