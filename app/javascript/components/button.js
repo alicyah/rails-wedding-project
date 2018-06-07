ff8378e9d72dd6a189d7c8ec892f0f7c887049f3
Pierre Cohen <pierre-cohen@hotmail.fr>
2018-06-07 13:18:57 +0200

    js is finally done brosssss

--- app/javascript/components/button.js
+++ app/javascript/components/button.js
@@ -1,56 +1,27 @@
-function mouseOver(lock) {
-  lock.addEventListener("mouseover", (event) => {
-    lock.classList.add('hidden');
-    const lockIcon = lock.querySelector('i');
-    const animation = lock.parentNode.querySelector('.supplier-lock-animation');
-    const animationIcon = animation.querySelector('i');
-    animation.classList.remove('hidden');
-    if (lockIcon.classList.contains("fa-lock-open")) {
-      animationIcon.classList.remove("fa-lock-open");
-      animationIcon.classList.add("fa-lock");
-    } else {
-      animationIcon.classList.remove("fa-lock");
-      animationIcon.classList.add("fa-lock-open");
-    }
-  })
-};
-function mouseOut(animationLock) {
-  animationLock.addEventListener("mouseout", (event) => {
-    animationLock.classList.add('hidden');
-    const lock = animationLock.parentNode.querySelector('.supplier-lock');
-    lock.classList.remove('hidden');
-  });
-};
-
 function updateTotalAmount() {
   // ajouter une classe aux lockers principaux
   // const lockedIcons = document.querySelectorAll('.hidden .fa-lock')
-
   const lockedIcons = document.querySelectorAll('.card-supplier-image .supplier-lock .fa-lock');
   const amount = document.getElementById('amount');
   let totalPrice = parseFloat(amount.dataset.amount);
-
   lockedIcons.forEach((icon) => {
     const card = icon.closest('.card-supplier');
     const priceCard = parseInt(card.querySelector('.card-price').innerText.replace(/\s/g, ''));
     totalPrice += priceCard;
   });
-
   amount.innerText = totalPrice;
 }
 
-function clickingLock(animationLock) {
-  animationLock.addEventListener("click", (event) => {
-    const animationIcon = animationLock.querySelector('i');
-    const lock = animationLock.parentNode.querySelector('.supplier-lock');
+function clickingLock(lock) {
+  lock.addEventListener("click", (event) => {
     const lockIcon = lock.querySelector('i');
-    if (animationIcon.classList.contains("fa-lock-open")) {
-      lockIcon.classList.add("fa-lock-open");
-      lockIcon.classList.remove("fa-lock");
+    if (lockIcon.classList.contains("fa-lock-open")) {
+      lockIcon.classList.remove("fa-lock-open");
+      lockIcon.classList.add("fa-lock");
       toggleServicesAvailability(lock);
     } else {
-      lockIcon.classList.add("fa-lock");
-      lockIcon.classList.remove("fa-lock-open");
+      lockIcon.classList.remove("fa-lock");
+      lockIcon.classList.add("fa-lock-open");
       toggleServicesAvailability(lock);
     }
     updateTotalAmount()
@@ -58,13 +29,42 @@ function clickingLock(animationLock) {
 };
 
 function toggleServicesAvailability(lock) {
-  // get service category from lock
-  const serviceImage = lock.dataset.service;
-  console.log(serviceImage);
-  // select all cards of that category except current card
-
-  // toggle lock display
+  const service = lock.dataset.service;
+  const supplierId = lock.dataset.supplierId;
+  const locks = document.querySelectorAll(`#${service} .supplier-lock`);
+  locks.forEach((lockTwo) => {
+    if (lockTwo.dataset.supplierId != supplierId) {
+      lockTwo.classList.toggle('hidden');
+    }
+  });
 }
+
+function lockingLock() {
+  const locks = document.querySelectorAll(".supplier-lock");
+  locks.forEach((lock) => {
+      clickingLock(lock);
+    })
+}
+
+export { lockingLock };
+
+
+
+//part of former lockingLock function
+  // if (locks) {
+  //   locks.forEach((lock) => {
+  //     mouseOver(lock);
+  //   })
+  // }
+  // const animationLocks = document.querySelectorAll(".supplier-lock-animation");
+  // if (animationLocks) {
+  //   animationLocks.forEach((animationLock) => {
+  //     mouseOut(animationLock);
+  //     clickingLock(animationLock);
+  //   })
+  // }
+  // onlyOneLock();
+//
 
 // function onlyOneLock() {
 //   const locked = document.querySelector(".fa-lock")
@@ -80,22 +80,26 @@ function toggleServicesAvailability(lock) {
 //     })
 //   }
 // }
-
-function lockingLock() {
-  const locks = document.querySelectorAll(".supplier-lock");
-  if (locks) {
-    locks.forEach((lock) => {
-      mouseOver(lock);
-    })
-  }
-  const animationLocks = document.querySelectorAll(".supplier-lock-animation");
-  if (animationLocks) {
-    animationLocks.forEach((animationLock) => {
-      mouseOut(animationLock);
-      clickingLock(animationLock);
-    })
-  }
-  // onlyOneLock();
-}
-
-export { lockingLock };
+// function mouseOver(lock) {
+//   lock.addEventListener("mouseover", (event) => {
+//     lock.classList.add('hidden');
+//     const lockIcon = lock.querySelector('i');
+//     const animation = lock.parentNode.querySelector('.supplier-lock-animation');
+//     const animationIcon = animation.querySelector('i');
+//     animation.classList.remove('hidden');
+//     if (lockIcon.classList.contains("fa-lock-open")) {
+//       animationIcon.classList.remove("fa-lock-open");
+//       animationIcon.classList.add("fa-lock");
+//     } else {
+//       animationIcon.classList.remove("fa-lock");
+//       animationIcon.classList.add("fa-lock-open");
+//     }
+//   })
+// };
+// function mouseOut(animationLock) {
+//   animationLock.addEventListener("mouseout", (event) => {
+//     animationLock.classList.add('hidden');
+//     const lock = animationLock.parentNode.querySelector('.supplier-lock');
+//     lock.classList.remove('hidden');
+//   });
+// };
